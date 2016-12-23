d314231d20d0e2a85eb18f9625a36a38e78a2d64
Brent Perry <burentoperii@gmail.com>
2016-12-23 12:08:01 -0600

    create new Account object and display initial balance.

--- js/scripts.js
+++ js/scripts.js
@@ -1,97 +1,18 @@
-function Contact(first, last) {
-  this.firstName = first;
-  this.lastName = last;
-  this.addresses = [];
-}
-
-function Address(type, street, city, state) {
-  this.type = type;
-  this.street = street;
-  this.city = city;
-  this.state = state;
-}
-
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
-}
-
-Address.prototype.fullAddress = function() {
-  return this.type + ": " + this.street + ", " + this.city + ", " + this.state;
-}
-
-function resetFields() {
-  $("input#new-first-name").val("");
-  $("input#new-last-name").val("");
-  $("input.new-address-type").val("");
-  $("input.new-street").val("");
-  $("input.new-city").val("");
-  $("input.new-state").val("");
-  $(".new-address").not(document.getElementById("initial-address")).remove();
-}
-
-function showContactDetails(contact) {
-  $("#show-contact h2").text(contact.firstName);
-  $(".first-name").text(contact.firstName);
-  $(".last-name").text(contact.lastName);
-  $("ul#addresses").text("");
-  contact.addresses.forEach(function(address) {
-    $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-  });
-  $("#show-contact").fadeIn();
+function Account(name, deposit) {
+  this.name = name;
+  this.balance = deposit;
 }
 
 $(document).ready(function() {
-  $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-address-type">Address type (e.g. work, home, etc.)</label>' +
-                                   '<input type="text" class="form-control new-address-type">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-street">Street</label>' +
-                                   '<input type="text" class="form-control new-street">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-city">City</label>' +
-                                   '<input type="text" class="form-control new-city">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-state">State</label>' +
-                                   '<input type="text" class="form-control new-state">' +
-                                 '</div>' +
-                               '</div>');
-  });
-  $("form#new-contact").submit(function(event) {
+  $("form#new-account").submit(function(event) {
     event.preventDefault();
+    var submittedName = $("#new-name").val();
+    var submittedInitialDeposit = parseInt($("#initial-deposit").val());
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
+    var newAccount = new Account(submittedName, submittedInitialDeposit);
+    $("#balance").text("$" + newAccount.balance.toFixed(2));
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
-
-    $(".new-address").each(function() {
-      var inputtedType = $(this).find("input.new-address-type").val();
-      var inputtedStreet = $(this).find("input.new-street").val();
-      var inputtedCity = $(this).find("input.new-city").val();
-      var inputtedState = $(this).find("input.new-state").val();
-      var newAddress = new Address(inputtedType, inputtedStreet, inputtedCity, inputtedState);
-      newContact.addresses.push(newAddress);
-    });
-
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
-    $(".contact").hover(
-      function() {
-        $(this).addClass("hover");
-      }, function() {
-        $(this).removeClass("hover");
-      }
-    );
-
-    $(".contact").last().click(function() {
-      showContactDetails(newContact);
-    });
-
-    resetFields();
+    $("#new-name").val("");
+    $("#initial-deposit").val("");
   });
 });
